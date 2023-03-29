4b4dca9de710027a9652746314ed5b5590e0cff6
Do Van Hoang <dohoang790@gmail.com>
2023-03-29 16:25:45 +0700

    giao dien cơ ban

--- js/main.js
+++ js/main.js
@@ -1,17 +1,143 @@
 var arrayNumber = [];
+var arrSoThuc = []; 
+
+//lấy đối tượng chưa thông báo lỗi cho trường nhập liệu
+var errorInput = document.querySelector('#error-input');
+
 document.getElementById('addNumber').onclick = function(){
     var str = document.getElementById('nhap-so').value;
     var arr = convertArrToNumber(str.split(" "));
+    if(!validateIntergerArray(arr)){
+        errorInput.style.display = 'block';
+        return;
+    }
+    else{
+        errorInput.style.display = 'none';
+
+    }
     arrayNumber = arrayNumber.concat(arr);
     console.log(arrayNumber);
     displayArr(arrayNumber);
+    document.querySelector('#nhap-so').value = '';
+    analysisArr(arrayNumber);
 }
+
+document.querySelector('#resetNumber').onclick =  function(){
+    arrayNumber = [];
+    document.querySelector('#addNumber').value = '';
+    analysisArr(arrayNumber);
+    displayArr(arrayNumber);
+}
+document.querySelector('#tang-dan').onclick = function(){
+    arrayNumber = sortIncrease(arrayNumber);
+    displayArr(arrayNumber);
+    analysisArr(arrayNumber);
+}
+document.querySelector('#giam-dan').onclick = function(){
+    arrayNumber = sortDecrease(arrayNumber);
+    displayArr(arrayNumber);
+    analysisArr(arrayNumber);
+}
+document.querySelector('#doiViTri').onclick = function(){
+    let index1 = +document.querySelector('#vi-tri-1').value;
+    let index2 = +document.querySelector('#vi-tri-2').value;
+    arrayNumber = changeIndex(index1,index2,arrayNumber);
+    displayArr(arrayNumber);
+}
+
+document.querySelector('#tim-so-nguyen-to').onclick = function(){
+    var soNguyenToDau = getPrimeFrom(arrayNumber)[0];
+    document.querySelector("#so-nguyen-to-dau").innerHTML = soNguyenToDau;
+}
+document.querySelector('#them-mang-so-thuc').onclick = function(){
+    var displaySoThuc = document.querySelector('#mang-so-thuc-output');
+    var arrStrSoThuc = document.getElementById('mang-so-thuc').value.split(' ');
+    var errorSoThuc = document.querySelector('#error-input-so-thuc');
+    if(!validateNumberArray(arrStrSoThuc)){
+        errorSoThuc.style.display = 'block';
+        return;
+    }
+    errorSoThuc.style.display = 'none';
+    var arrSoThucNew = convertArrToNumber(arrStrSoThuc);
+    document.querySelector('#mang-so-thuc').value = '';
+    arrSoThuc = arrSoThuc.concat(arrSoThucNew);
+    displaySoThuc.innerHTML = arrSoThuc.toString();
+}
+
+document.querySelector('#dem-so-nguyen').onclick = function(){
+    var soSoNguyen = getIntergerFrom(arrSoThuc).length;
+    document.querySelector('#so-so-nguyen').innerHTML = soSoNguyen;
+}
+
 
 
 
 function displayArr(arr){
     document.getElementById('hien-thi-mang').innerHTML = arr.toString();
 }
+
+function analysisArr(arr){
+    //output
+    let tongSoDuong = 0;
+    let soMin = 0;
+    let soDuongMin = 0;
+    let soChanLast = -1;
+    
+    //tinh tổng các số dương 
+    var arrSoDuong = getPositiveNumbers(arr);
+    for (let i = 0; i < arrSoDuong.length; i++) {
+        tongSoDuong += arrSoDuong[i];
+    }
+    //tìm số nhỏ nhất
+    soMin = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if(soMin>arr[i]){
+            soMin = arr[i];
+        }
+    }
+    //Tìm số dương nhỏ nhất
+    soDuongMin = arrSoDuong[0];
+    for (let i = 0; i < arrSoDuong.length; i++) {
+        if(soDuongMin>arrSoDuong[i]){
+            soDuongMin = arrSoDuong[i];
+        }
+    }
+    // lấy số chẳn cuối cùng
+    var soChan = getEvens(arr);
+    console.log(soChan);
+    if(soChan.length>0){
+        soChanLast = soChan[soChan.length-1];
+    }
+    // xử lý so sánh số lượng số âm và dương
+    var soDuong = getPositiveNumbers(arr).length;
+    console.log(soDuong);
+    var soAm = getNegativeNumbers(arr).length;
+    console.log(soAm);
+
+    var duongPerc = +Math.round((soDuong/(soAm+soDuong))*100);
+    var amPerc = 100 - duongPerc;
+    
+    //hiển thi ra màn hình
+    document.querySelector('#tong-so-duong').innerHTML = tongSoDuong;
+    document.querySelector('#so-luong-so-duong').innerHTML = arrSoDuong.length;
+    document.querySelector('#so-luong-so-duong').innerHTML = arrSoDuong.length;
+    document.querySelector('#so-min').innerHTML = soMin;
+    document.querySelector('#so-duong-min').innerHTML = soDuongMin;
+    document.querySelector('#so-chan-last').innerHTML = soChanLast;
+
+    //Hiên thị biểu đồ số âm dương
+    var duongChar = document.querySelector('#charDuong');
+    var amChar = document.querySelector('#charAm');
+    duongChar.innerHTML = duongPerc + '%';
+    amChar.innerHTML = amPerc + '%';
+    duongChar.style.width = duongPerc*4 + 'px'; 
+    amChar.style.width = amPerc*4 + 'px'; 
+}
+
+
+
+
+
 
 
 
--- utilities/arrayAnalysis.js
+++ utilities/arrayAnalysis.js
@@ -1,16 +1,16 @@
 function convertArrToNumber(arr){
     var arrTemp = [];
     for(let i = 0; i < arr.length; i++){
-        arrTemp[i] = parseInt(arr[i]);
+        arrTemp.push(parseFloat(arr[i]));
     }
     return arrTemp;
 }
 
 function getPositiveNumbers(arr){
-    let arrTemp = [];
+    var arrTemp = [];
     for(let i = 0; i < arr.length; i++){
-        if(arr[i]>=0){
-            arrTemp.push = arr[i];
+        if(arr[i]>0){
+            arrTemp.push(arr[i]);
         }
     }
     return arrTemp;
@@ -19,7 +19,7 @@ function getNegativeNumbers(arr){
     let arrTemp = [];
     for(let i = 0; i < arr.length; i++){
         if(arr[i]<0){
-            arrTemp.push = arr[i];
+            arrTemp.push(arr[i]);
         }
     }
     return arrTemp;
@@ -37,7 +37,7 @@ function getEvens(arr){
     let arrTemp = [];
     for (let i = 0; i < arr.length; i++) {
         if(arr[i]%2===0){
-            arrTemp.push = arr[i];
+            arrTemp.push(arr[i]);
         }
     }
     return arrTemp;
@@ -45,25 +45,106 @@ function getEvens(arr){
 
 function sortIncrease(arr){
     let tempt = 0;
-    for (let i = 0; i < arr.length -1; i++) {
-        for (let j = i+1; j < arr.length; j++) {
-            if(arr[i]>arr[j]){
-                tempt = arr[j];
-                arr[j] = arr[i];
-                arr[i] = arr[j];
+    var arrSort = arr;
+    for (let i = 0; i < arrSort.length -1; i++) {
+        for (let j = i+1; j < arrSort.length; j++) {
+            if(arrSort[i]>arrSort[j]){
+                tempt = arrSort[j];
+                arrSort[j] = arrSort[i];
+                arrSort[i] = tempt;
             }
         }
     }
+    return arrSort;
 }
 function sortDecrease(arr){
     let tempt = 0;
-    for (let i = 0; i < arr.length -1; i++) {
-        for (let j = i+1; j < arr.length; j++) {
-            if(arr[i]<arr[j]){
-                tempt = arr[j];
-                arr[j] = arr[i];
-                arr[i] = arr[j];
+    var arrSort = arr;
+    for (let i = 0; i < arrSort.length -1; i++) {
+        for (let j = i+1; j < arrSort.length; j++) {
+            if(arrSort[i]<arrSort[j]){
+                tempt = arrSort[j];
+                arrSort[j] = arrSort[i];
+                arr[i] = tempt;
             }
         }
     }
+    return arrSort; 
+}
+
+function changeIndex(targetIndex, index, arr){
+    if(targetIndex>=arr.length||index>=arr.length){
+        console.log('error our of range');
+        return -1;
+    }
+    let temp = 0;
+    temp = arr[index];
+    arr[index] = arr[targetIndex];
+    arr[targetIndex] = temp;
+    return arr;
+}
+
+function validateIntergerArray(arr){
+    for (let i = 0; i < arr.length; i++) {
+        if(!Number.isSafeInteger(arr[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+function getIntergerFrom(arr){
+    var arrInter = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(Number.isSafeInteger(arr[i])){
+            arrInter.push(arr[i]);
+        }
+    }
+    return arrInter;
+}
+
+function validateNumberArray(strArr){
+    for (let i = 0; i < strArr.length; i++) {
+        if(isNaN(strArr[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+function getPrimeFrom(arr){
+    var primes = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(kiem_tra_snt(arr[i])){
+            primes.push(arr[i]);
+        }
+    }
+    return primes;
+}
+
+function kiem_tra_snt(n)
+{
+    // Biến cờ hiệu
+    var flag = true;
+    // Nếu n bé hơn 2 tức là không phải số nguyên tố
+    if (n < 2){
+        flag = false;
+    }
+    else if (n == 2){
+        flag = true;
+    }
+    else if (n % 2 == 0){
+      flag = false;
+    }
+    else{
+        // lặp từ 3 tới n-1 với bước nhảy là 2 (i+=2)
+        for (var i = 3; i < Math.sqrt(n); i+=2)
+        {
+            if (n % i == 0){
+                flag = false;
+                break;
+            }
+        }
+    }
+    return flag;
 }
